5f32dbdc33696213a542ac03d738b58f1d38a90f
Brian <briansimiyu@gmail.com>
2024-09-23 20:31:19 +0300

    Changed Navbar folder to uppercase

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Navbar from './components/navbar/Navbar'
+import Navbar from './components/Navbar/Navbar'
 import  Hero  from './components/Hero/Hero'
 import Program from './components/Programs/Program'
 import Title from './components/Title/Title'
